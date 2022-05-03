e519cd09d1ab856c64ba8b53b81870ff0d65e29a
Quachbaohung1 <baohung9715@gmail.com>
2022-05-03 11:11:19 +0700

    Hung_BorutoDatabase+HeroRemoteKeysDao+RemoteDataSourceImpl+BorutoApi+HeroRemoteMediator

---
+++ app/src/main/java/dev/amal/borutoapp/domain/model/HeroRemoteKeys.kt
@@ -0,0 +1,13 @@
+package dev.amal.borutoapp.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import dev.amal.borutoapp.util.Constants.HERO_REMOTE_KEYS_DATABASE_TABLE
+
+@Entity(tableName = HERO_REMOTE_KEYS_DATABASE_TABLE)
+data class HeroRemoteKeys(
+    @PrimaryKey(autoGenerate = false)
+    val id: Int,
+    val prevPage: Int?,
+    val nextPage: Int?
+)
